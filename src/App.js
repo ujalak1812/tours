84e79693a69aac177482c49b87697fa338c11605
ujalakhurram <ujala.khurram@venturedive.com>
2021-06-29 13:14:11 +0500

    tours v1

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+
+      // converting response to json
+      const tours = await response.json();
+      setLoading(false);  //end loading
+      setTours(tours);
+      console.log(tours);
+    } 
+    catch(error) {
+      setLoading(false);
+    }
+  };
+
+  //To invoke the fetchTours() once the App component is rendered completely
+  useEffect (() => {
+    fetchTours();
+  },[]);  //to run it only once on initial render
+ 
+  if(loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if(tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={fetchTours}>refresh</button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App
---
+++ src/Tour.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMode] = useState(false);
+
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0,200)}...`}
+          <button onClick={() => setReadMode(!readMore)}>
+            {readMore ? 'show less' : 'read more'}
+          </button>
+        </p>
+        <button className='delete-btn' onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
